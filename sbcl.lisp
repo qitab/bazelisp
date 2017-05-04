de1488496d42949c5332dd4edac28f664262e62e
Common Lisp at Google <common-lisp-dev-copybara@google.com>
2017-05-04 16:05:09 -0400

    Internal change
    PiperOrigin-RevId: 155115924

--- sbcl.lisp
+++ sbcl.lisp
@@ -290,6 +290,7 @@
     (precompile-generic-functions :verbose bazel.log:*verbose*))
   (unless verbose (mute-output-streams))
   (sb-ext:gc :full t)
+  (setf (extern-alien "gc_coalesce_string_literals" char) 1)
   (sb-ext:save-lisp-and-die
    name :executable t :compression compression :toplevel toplevel
         :save-runtime-options save-runtime-options)
