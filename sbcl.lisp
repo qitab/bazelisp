4c1589fd5db944447a38dbb89568513755b236a9
Common Lisp at Google <common-lisp-dev-copybara@google.com>
2018-09-28 13:51:37 -0400

    Internal change
    PiperOrigin-RevId: 214960913

--- sbcl.lisp
+++ sbcl.lisp
@@ -147,8 +147,11 @@
      (declare (ignore size))
      (when (= type sb-vm:code-header-widetag)
        (dotimes (i (sb-kernel:code-n-entries obj))
-         (let ((f (sb-kernel:%code-entry-point obj i)))
-           (setf (sb-kernel:%simple-fun-info f) nil)))))
+         (let* ((f (sb-kernel:%code-entry-point obj i))
+                (info (sb-kernel:%simple-fun-info f)))
+           ;; Preserve source forms, assuming we want them if they exist.
+           (sb-kernel:set-simple-fun-info
+            f (sb-kernel:%simple-fun-lexpr f) nil nil)))))
    :all))
 
 ;;;
